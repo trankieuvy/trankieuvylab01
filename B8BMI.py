07c05e7368523b7d1045ff1bd973eecf3c5171c3
trankieuvy <trankieuvy2005@gmail.com>
2024-08-30 14:40:34 +0700

    B8.py

--- B8BMI.py
+++ B8BMI.py
@@ -15,6 +15,14 @@ a_m=a/100
 BMI= b/a_m**2
 
 print("Chỉ số BMI của bạn là: ", round(BMI,1))
+if BMI<18.5:
+    print("Gầy: Cơ thể thiếu cân, cần tăng cường dinh dưỡng và luyện tập để đạt cân nặng lý tưởng.")
+if 18.5 < BMI < 24.9:
+    print("Bình thường: Cân nặng phù hợp với chiều cao, bạn đang ở mức lý tưởng.")
+if 25<BMI<29.9:
+    print("Thừa cân: Cần điều chỉnh chế độ ăn và tăng cường hoạt động thể chất để giảm cân.")  
+if BMI>30:
+    print("Béo phì: Nguy cơ cao mắc các bệnh liên quan đến béo phì, cần có kế hoạch giảm cân cụ thể và khoa học.")
 
       
 
